ca41f4a2a7a7e8877af7f9842face67c3dcd1638
JIHO\82108 <rlawlghwk@naver.com>
2024-05-15 23:20:39 +0900

    feat 패키지 생성 및 인터페이스 만드는중 #1

---
+++ src/player/playerInter.java
@@ -0,0 +1,6 @@
+package player;
+
+public interface playerInter {
+	public String getName();
+	public String getRps();
+}
---
+++ src/player/userPlayer.java
@@ -0,0 +1,19 @@
+package player;
+
+public class userPlayer implements playerInter {
+	private final String name;
+	private final String rpsType;
+	
+	
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String getRps() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
